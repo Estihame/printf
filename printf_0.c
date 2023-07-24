9e6ae0f5db5e58ccbbfc7fce38f36e75d16bb8c9
Damilare Sotunde <damilaresotunde@yahoo.com>
2023-07-25 00:51:56 +0100

    code modified to handle a null condition

--- printf_0.c
+++ printf_0.c
@@ -18,9 +18,12 @@ int _printf(const char *format, ...)
 	int k;
 	char *str;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(output, format);
 	
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
